28b0a641a6c69cb39180646a6d7841b4640ad286
Johny270 <90469202+Johny270@users.noreply.github.com>
2024-01-18 14:27:58 -0400

    Pass data in as props to user and product page

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,10 +1,11 @@
 import Single from "../../components/Single.tsx/Single"
+import { singleUser } from "../../data"
 import "./user.scss"
 
 const User = () => {
   return (
     <div className="user">
-      <Single />
+      <Single {...singleUser} />
     </div>
   )
 }
