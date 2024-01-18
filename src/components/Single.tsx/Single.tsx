3dd7e91f82655f69036e6a8cb6fa98e9f78878f4
Johny270 <90469202+Johny270@users.noreply.github.com>
2024-01-18 14:25:00 -0400

    Set up single page component to receive data as props

--- src/components/Single.tsx/Single.tsx
+++ src/components/Single.tsx/Single.tsx
@@ -2,66 +2,36 @@ import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, XAxis, YAx
 import "./single.scss"
 import { Tooltip } from "@mui/material"
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+type Props = {
+  id: number;
+  img?: string;
+  title: string;
+  info: object;
+  chart?: {
+    dataKeys: { name: string; color: string}[];
+    data: object[];
+  };
+  activities?: { time: string; text: string }[];
+};
 
-const Single = () => {
+
+const Single = (props: Props) => {
   return (
     <div className="single">
       <div className="view">
         <div className="info">
           <div className="topInfo">
-            <img src="https://wallpapercave.com/dwp1x/wp7802221.jpg" alt="" />
-            <h1>John Doe</h1>
+            { props.img && <img src={props.img} alt="" /> }
+            <h1>{props.title}</h1>
             <button>Update</button>
           </div>
           <div className="details">
-            <div className="item">
-              <span className="itemTitle">Username: </span>
-              <span className="itemValue">John Doe</span>
-            </div>
+            {Object.entries(props.info).map((item) => (
+              <div className="item" key={item[0]}>
+                <span className="itemTitle">{item[0]}</span>
+                <span className="itemValue">{item[1]}</span>
+              </div>
+            ))}
             <div className="item">
               <span className="itemTitle">Username: </span>
               <span className="itemValue">John Doe</span>
@@ -73,57 +43,45 @@ const Single = () => {
           </div>
         </div>
         <hr />
-        <div className="chart">
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart
-              width={500}
-              height={300}
-              data={data}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
+          { props.chart && (
+            <div className="chart">
+              <ResponsiveContainer width="100%" height="100%">
+                <LineChart
+                  width={500}
+                  height={300}
+                  data={props.chart.data}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                  }}
+                >
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  {props.chart.dataKeys.map(dataKey => (
+                    <Line type="monotone" dataKey={dataKey.name} stroke={dataKey.color} />
+                  ))}
+                </LineChart>
+              </ResponsiveContainer>
+            </div>
+          )}
       </div>
       <div className="activities">
         <h2>Latest Activities</h2>
-        <ul>
-          <li>
-            <div>
-              <p>John Doe Purchased a Playstation 5</p>
-              <span>3 days ago.</span>
-            </div>
-          </li>
-          <li>
-            <div>
-              <p>John Doe Purchased a Playstation 5</p>
-              <span>3 days ago.</span>
-            </div>
-          </li>
-          <li>
-            <div>
-              <p>John Doe Purchased a Playstation 5</p>
-              <span>3 days ago.</span>
-            </div>
-          </li>
-          <li>
-            <div>
-              <p>John Doe Purchased a Playstation 5</p>
-              <span>3 days ago.</span>
-            </div>
-          </li>
-        </ul>
+        { props.activities && <ul>
+          {props.activities.map(activity => (
+            <li>
+              <div>
+                <p>{activity.text}</p>
+                <span>{activity.time}</span>
+              </div>
+            </li>
+          ))}
+          </ul>
+        }
       </div>
     </div>
   )
